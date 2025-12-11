f489a03cf1d3970697c101e47e243449b963e624
Rutuja Nemane <rutuja.b.nemane2808@gmail.com>
2025-12-10 19:25:08 -0800

    Change of port

--- catalog-api-testing/main.py
+++ catalog-api-testing/main.py
@@ -290,5 +290,5 @@ async def get_product_reviews(product_id: str, page: int = 1):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
 
